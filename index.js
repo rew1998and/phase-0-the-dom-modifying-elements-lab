90e18b81822e76ebcc3e032ce8f91a9aa0cbc0dc
Andrew <rew1998and@gmail.com>
2023-12-27 21:05:27 -0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Andrew is the champion";
+
+const element = document.createElement("div");
+document.body.append(element);
+const ul = document.createElement("ul");
+for (let i = 0; i < 3; i++) {
+const li = document.createElement("li");
+li.textContent = (i + 1).toString();
+ul.append(li);
+}
+element.append(ul);
+
+document.body.append(newHeader);
+
